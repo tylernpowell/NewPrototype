8b03b64c5238fdbe6af9d5e5a8e6c1a6a02993e0
krypticwolf3 <krypticwolf3@users.noreply.github.com>
2016-04-23 19:15:41 -0400

    Creating the second floor state.

---
+++ js/floor2.js
@@ -0,0 +1,14 @@
+var floor2 = function(game){}
+
+var numOfKeys;
+var envelopes;
+
+floor2.prototype = {
+  init: function(notes,numKeys){
+    numOfKeys = numKeys;
+    envolopes = notes;
+  },
+  create: function(){
+    
+  }
+}
