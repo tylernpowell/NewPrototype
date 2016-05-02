a6e2eb68d3e625a8e3b7c7704d3079ee1d36c06a
krypticwolf3 <krypticwolf3@users.noreply.github.com>
2016-05-01 21:26:29 -0400

    Commenting code.

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@ var init = function(game){};
 
 init.prototype = {
   preload: function(){
+    // Loading the image used for our loading screen.
     this.game.loadScreen.image("loading","assets/loading.png");
   },
   create: function(){
