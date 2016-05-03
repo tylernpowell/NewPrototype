f8806a6a5596861486ed8d27610abc5cad084334
tylernpowell <tpowell9@gmu.edu>
2016-05-03 05:46:16 -0400

    Start

---
+++ assets/Mansion.tsx
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mansion" tilewidth="64" tileheight="64" tilecount="21" columns="0">
+ <tile id="0">
+  <image width="32" height="32" source="BrickFloor.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="BrickWall.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="BrickWall2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="coin.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="64" source="door.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="16" source="door2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="Envelope.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="Key.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="Stairs_DU.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="Stairs_LR.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="Stairs_RL.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="Stairs_UD.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="CollapsedCeiling.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="CollapsedCeiling2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="CollapsedCeiling3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="CollapsedCeiling4.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="Door-vertical.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="Door-horizontal.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="Table-horizontal.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="Table-vertical.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="Cauldron.png"/>
+ </tile>
+</tileset>
