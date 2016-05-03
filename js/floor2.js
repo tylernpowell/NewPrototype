c1e33f068145e99e78b1810c00d60fc9dd68b106
krypticwolf3 <krypticwolf3@users.noreply.github.com>
2016-05-02 21:46:46 -0400

    Integrating states with game code.

--- js/floor2.js
+++ js/floor2.js
@@ -1,14 +1,26 @@
 var floor2 = function(game){}
 
-var numOfKeys;
-var envelopes;
-
 floor2.prototype = {
+  var player;
+  var layer;
+  var collideLayer;
+  var map;
+  var collisionMap;
+  var music;
+  var envelopes = [];
+  var keys = [];
+  var doors = [];
+  var enemy;
+    
   init: function(notes,numKeys){
     numOfKeys = numKeys;
     envolopes = notes;
   },
   create: function(){
     
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.gravity.y = 0;  
+        
+        game.stage.backgroundColor = '#000000'; 
   }
 }
